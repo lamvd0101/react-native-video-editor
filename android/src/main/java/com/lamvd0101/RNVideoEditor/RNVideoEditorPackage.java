f1677b2e560f3580b3d1ae548f0cc66671fbbe24
Vương Đức Lâm <lamvd0101@gmail.com>
2019-11-20 20:00:04 +0700

    Udpate version

--- android/src/main/java/com/lamvd0101/RNVideoEditor/RNVideoEditorPackage.java
+++ android/src/main/java/com/lamvd0101/RNVideoEditor/RNVideoEditorPackage.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class RnVideoEditorPackage implements ReactPackage {
+public class RNVideoEditorPackage implements ReactPackage {
   @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
     return Collections.emptyList();
