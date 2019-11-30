965b8fd192685a4488c81059a79f470f88e83c9c
Vuong Duc Lam <lamvd0101@gmail.com>
2019-11-30 17:58:23 +0700

    Fix bug ios

--- ios/RNVideoEditorUtilities.swift
+++ ios/RNVideoEditorUtilities.swift
@@ -10,74 +10,74 @@ import AVFoundation
 import Photos
 
 extension String: LocalizedError {
-  public var errorDescription: String? { return self }
+    public var errorDescription: String? { return self }
 }
 
 class RNVideoEditorUtilities {
-  static let filePrefix: String = "reaction-media"
-  
-  static func requestAsset(_ source: String) throws -> AVAsset {
-    var url: URL?
-    var avAsset: AVAsset?
-    if source.contains("ph://") {
-      let ids: Array = [source.replacingOccurrences(of: "ph://", with: "")]
-      let assets: PHFetchResult = PHAsset.fetchAssets(withLocalIdentifiers: ids, options: nil)
-      let phAsset: PHAsset? = assets.firstObject
-      guard phAsset != nil else { throw "Failed to request asset." }
-      guard (phAsset!.mediaType == .video) else { throw "Failed to request asset." }
-      
-      let semaphore: DispatchSemaphore = DispatchSemaphore(value: 0)
-      let options: PHVideoRequestOptions = PHVideoRequestOptions()
-      options.deliveryMode = .fastFormat
-      options.isNetworkAccessAllowed = true
-      
-      PHCachingImageManager().requestAVAsset(
-        forVideo: phAsset!,
-        options: options) { (asset, _, _) in
-          avAsset = asset
-          semaphore.signal()
-      }
-      semaphore.wait()
-    } else if source.contains("assets-library") {
-      url = NSURL(string: source)! as URL
-    } else {
-      url = URL(string: source, relativeTo: Bundle.main.resourceURL)
-    }
-    if url != nil { avAsset = AVAsset(url: url!) }
-    guard avAsset != nil else { throw "Failed to request asset." }
-
-    return avAsset!
-  }
-  
-  static func createTempFile(_ fileExtension: String) throws -> URL {
-    do {
-      let fileName: String = ProcessInfo.processInfo.globallyUniqueString
-      let fileManager: FileManager = FileManager.default
-      
-      let dir: URL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
-      let url: URL = dir.appendingPathComponent("\(RNVideoEditorUtilities.filePrefix)-\(fileName).\(fileExtension)")
-      
-      try fileManager.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
-      try fileManager.removeItem(atPath: url.path)
-      
-      return url;
-    } catch {
-      throw "Failed to create temp file."
-    }
-  }
-  
-  static func cleanFiles() throws -> Void {
-    do {
-      let fileManager: FileManager = FileManager.default
-      let dir: URL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
-      let files: [URL] = try fileManager.contentsOfDirectory(at: dir, includingPropertiesForKeys: nil, options: [])
-      for file in files {
-        if file.lastPathComponent.contains(RNVideoEditorUtilities.filePrefix) {
-          try fileManager.removeItem(atPath: file.path)
+    static let filePrefix: String = "reaction-media"
+    
+    static func requestAsset(_ source: String) throws -> AVAsset {
+        var url: URL?
+        var avAsset: AVAsset?
+        if source.contains("ph://") {
+            let ids: Array = [source.replacingOccurrences(of: "ph://", with: "")]
+            let assets: PHFetchResult = PHAsset.fetchAssets(withLocalIdentifiers: ids, options: nil)
+            let phAsset: PHAsset? = assets.firstObject
+            guard phAsset != nil else { throw "Failed to request asset." }
+            guard (phAsset!.mediaType == .video) else { throw "Failed to request asset." }
+            
+            let semaphore: DispatchSemaphore = DispatchSemaphore(value: 0)
+            let options: PHVideoRequestOptions = PHVideoRequestOptions()
+            options.deliveryMode = .fastFormat
+            options.isNetworkAccessAllowed = true
+            
+            PHCachingImageManager().requestAVAsset(
+                forVideo: phAsset!,
+                options: options) { (asset, _, _) in
+                    avAsset = asset
+                    semaphore.signal()
+            }
+            semaphore.wait()
+        } else if source.contains("assets-library") {
+            url = NSURL(string: source)! as URL
+        } else {
+            url = URL(string: source, relativeTo: Bundle.main.resourceURL)
         }
-      }
-    } catch  {
-      throw "Failed to clean files."
+        if url != nil { avAsset = AVAsset(url: url!) }
+        guard avAsset != nil else { throw "Failed to request asset." }
+        
+        return avAsset!
     }
-  }
+    
+    static func createTempFile(_ fileExtension: String) throws -> URL {
+        do {
+            let fileName: String = ProcessInfo.processInfo.globallyUniqueString
+            let fileManager: FileManager = FileManager.default
+            
+            let dir: URL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+            let url: URL = dir.appendingPathComponent("\(RNVideoEditorUtilities.filePrefix)-\(fileName).\(fileExtension)")
+            
+            try fileManager.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+            try fileManager.removeItem(atPath: url.path)
+            
+            return url;
+        } catch {
+            throw "Failed to create temp file."
+        }
+    }
+    
+    static func cleanFiles() throws -> Void {
+        do {
+            let fileManager: FileManager = FileManager.default
+            let dir: URL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+            let files: [URL] = try fileManager.contentsOfDirectory(at: dir, includingPropertiesForKeys: nil, options: [])
+            for file in files {
+                if file.lastPathComponent.contains(RNVideoEditorUtilities.filePrefix) {
+                    try fileManager.removeItem(atPath: file.path)
+                }
+            }
+        } catch  {
+            throw "Failed to clean files."
+        }
+    }
 }
